e9422142dba54a02cfc4c222cefd76dc3574641a
Akshar95 <patel.akshar295@gmail.com>
2022-03-20 11:19:00 +0000

    updated login with get server side function

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import { getProviders, signIn } from "next-auth/react";
+//need to do SSR to get all of the providers from [...nextauth] line 34 before this page loads
 
 function Login() {
   return (
     <div>
       <h1>This is the login page</h1>
     </div>
-  )
+  );
+};
+
+export default Login;
+
+export async function getServerSideProps(){
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
 }
 
-export default Login
